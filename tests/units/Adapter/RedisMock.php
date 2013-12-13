dd6753c56f9f678393517cc2273cef39d283b740
Olivier Mansour <olivier.mansour@m6.fr>
2013-12-14 00:09:21 +0100

    test adaptation

--- tests/units/Adapter/RedisMock.php
+++ tests/units/Adapter/RedisMock.php
@@ -17,11 +17,25 @@ class RedisMockAdapter extends test
     public function testMock()
     {
         Base::getMock('\StdClass');
-        $mock = new \mock\M6Web\Component\RedisMock\pseudoRedisMock;
+        $mock = new \mock\M6Web\Component\RedisMock\StdClass;
         $this->assert
             ->object($mock)
                 ->isInstanceOf('\StdClass')
-            ->class('\mock\M6Web\Component\RedisMock\pseudoRedisMock')
+            ->class('\mock\M6Web\Component\RedisMock\StdClass')
+                ->hasMethod('reset')
+                ->hasMethod('set')
+                ->hasMethod('get')
+                ->hasMethod('zrevrangebyscore');
+    }
+
+    public function testNamedMock()
+    {
+        Base::getMock('\StdClass', 'raoul');
+        $mock = new \mock\M6Web\Component\RedisMock\raoul;
+        $this->assert
+            ->object($mock)
+                ->isInstanceOf('\StdClass')
+            ->class('\mock\M6Web\Component\RedisMock\raoul')
                 ->hasMethod('reset')
                 ->hasMethod('set')
                 ->hasMethod('get')
